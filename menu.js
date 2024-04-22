d9b46a88d8a6026522c837b3fdf9ed9dc8cd349e
cleberpb <36317652+cleberpb@users.noreply.github.com>
2024-04-22 11:30:56 -0300

    animação javascript

---
+++ menu.js
@@ -0,0 +1,6 @@
+let btnmenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+
+btnmenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
